8145f1eacc211bc7b67af9fab3a5c15436b1ddd1
Vilhelm <vilhelm.lindell123@gmail.com>
2024-04-06 21:42:08 +0200

    stuff

---
+++ config/plugins/utils/zellij.nix
@@ -0,0 +1,11 @@
+{
+  plugins.zellij = {
+    enable = true;
+    settings = {
+      debug = true;
+      path = "zellij";
+      replaceVimWindowNavigationKeybinds = false;
+      vimTmuxNavigatorKeybinds = true;
+    };
+  };
+}
